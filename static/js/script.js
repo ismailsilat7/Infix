9da7195def0b575d8d4bfb8f78a2dcefd7c044d3
ismailsilat7 <ismailsilat7@gmail.com>
2024-08-05 02:12:11 +0500

    Add class toggle for hamburger menu

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+const navE1 = document.querySelector('.nav');
+const hamburgerE1 = document.querySelector('.hamburger')
+
+
+hamburgerE1.addEventListener('click', () => {
+    navE1.classList.toggle("nav--open");
+    hamburgerE1.classList.toggle("hamburger--open")
+});
+
+navE1.addEventListener('click', () => {
+    navE1.classList.remove("nav--open");
+    hamburgerE1.classList.remove("hamburger--open")
+})
