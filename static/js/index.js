cb80c814393c5ed78ba95c6d3d32bbbaea72e451
Ali Kashif <alikashif5917@gmail.com>
2024-08-07 09:33:23 +0500

    add nav hidden in intro

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,15 @@
 var copy = document.querySelector(".logos-slide").cloneNode(true)
 document.querySelector('.logos').appendChild(copy)
+
+function checkSection() {
+  var navbar = document.getElementById('navbar');
+  var introSection = document.getElementById('intro-section');
+  var introRect = introSection.getBoundingClientRect();
+  if (introRect.top <= window.innerHeight && introRect.bottom >= 0) {
+    navbar.style.display = 'none';
+  } else {
+    navbar.style.display = 'block';
+  }
+}
+window.addEventListener('load', checkSection);
+window.addEventListener('scroll', checkSection);
