1088954350b39a2ca55b771a316c12c6bb6b6d42
ismailsilat7 <ismailsilat7@gmail.com>
2024-08-08 03:53:28 +0500

    Add queries used in app.py

---
+++ data/log.sql
@@ -0,0 +1,10 @@
+SELECT email FROM users WHERE email = ?
+
+SELECT nickname FROM users WHERE nickname = ?
+
+INSERT INTO users (fullname, nickname, email, hash) VALUES (?,?,?,?)
+
+UPDATE users SET hash = ? WHERE email = ?
+
+
+
