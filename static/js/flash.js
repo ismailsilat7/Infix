e6c3189f05c810fb3eb278a8d4f71be7a38e9248
ismailsilat7 <ismailsilat7@gmail.com>
2024-08-12 21:40:15 +0500

    update js for flash

--- static/js/flash.js
+++ static/js/flash.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const alertButtons = document.querySelectorAll('.alert .close');
+    const alertButtons = document.querySelectorAll('.alert #close');
     alertButtons.forEach(button => {
         button.addEventListener('click', () => {
         button.parentElement.style.display = 'none';
