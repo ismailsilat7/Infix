973b9901ba2a5583764e2fc48a408b6b948524f4
ismailsilat7 <ismailsilat7@gmail.com>
2024-08-12 20:15:42 +0500

    Add js for close button

---
+++ static/js/flash.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const alertButtons = document.querySelectorAll('.alert .close');
+    alertButtons.forEach(button => {
+        button.addEventListener('click', () => {
+        button.parentElement.style.display = 'none';
+        });
+    });
+});
