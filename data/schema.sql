a4a86ed33a18bcbe478b4473b04694508da4b004
Ali Kashif <alikashif5917@gmail.com>
2024-08-19 00:31:30 +0500

    add seq_num column in topics

--- data/schema.sql
+++ data/schema.sql
@@ -51,6 +51,7 @@ CREATE TABLE IF NOT EXISTS topics (
     course_id INTEGER NOT NULL,
     title TEXT NOT NULL,
     category_id INTEGER NOT NULL,
+    seq_num INTEGER NOT NULL DEFAULT 0;
     FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
     FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
 );
